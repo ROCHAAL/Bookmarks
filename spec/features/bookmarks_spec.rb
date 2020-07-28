78838ca1e49bf337f274e5193e995853466cfdc9
Patricia <prmonday@gmail.com>
2020-07-28 17:57:47 +0100

    03_viewing_bookmarks.md

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,6 +1,13 @@
-feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content "Hello World"
+require 'bookmark'
+describe Bookmark do
+  describe '.all' do
+    it 'returns all bookmarks' do
+      bookmarks = Bookmark.all
+
+
+      expect(bookmarks).to include("http://www.makersacademy.com")
+      expect(bookmarks).to include( "http://www.destroyallsoftware.com")
+      expect(page).to include("http://www.google.com")
+    end
   end
 end
