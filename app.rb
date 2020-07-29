94497a7e351cbe95ff9b70fc4420d97a4d0b7b47
Patricia <prmonday@gmail.com>
2020-07-29 15:26:58 +0100

    Set up the database

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require './lib/bookmark'
 class BookmarkManager < Sinatra::Base
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-      erb :'bookmarks/index'
+      erb :'index'
     end
 
   run! if app_file == $0
